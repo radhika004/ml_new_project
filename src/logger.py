2ffa1e7f8968555f829f2d08dc87fc553b2d9095
radhika004 <radhikachare2004@gmail.com>
2026-07-12 00:21:17 +0530

    exception and logger completed

---
+++ src/logger.py
@@ -0,0 +1,29 @@
+import logging
+import sys
+import os
+from datetime import datetime
+from src.exception import CustomException
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+if __name__ == "__main__":
+  
+  try:
+    a=1/0
+  except Exception as e:
+    
+    logging.info("Divide by Zero")
+    raise CustomException(e,sys)
+    
+  
