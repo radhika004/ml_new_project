b47270cdebf26d4381dcd208b538522a08f814ba
radhika004 <radhikachare2004@gmail.com>
2026-07-16 21:12:16 +0530

    preprocesing object

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -87,7 +87,26 @@ class DataTransformation:
       numerical_columns =["writing_score","reading_score"]
       
       input_feature_train_df = train_df.drop(columns=[target_column_name],axis=1)
+      target_feature_train_df = train_df[target_column_name]
       
+      
+      input_feature_test_df = test_df.drop(columns=[target_column_name],axis=1)
+      target_feature_test_df = test_df[target_column_name]
+      
+      logging.info("Applying preprocessing object on training and testing dataframe")
+      
+      input_feature_train_arr = preprocessor_obj.fit_transform(input_feature_train_df)
+      input_feature_test_arr = preprocessor_obj.transform(input_feature_test_df)
+      
+      train_arr =  np.c_[
+        input_feature_train_arr,np.array(target_feature_train_df)
+      ]
+      
+      test_arr = np.c_[
+        input_feature_test_arr,np.array(target_feature_test_df)
+      ]
+      
+      logging.info("Saved preprocessing object")
     except:
       pass
     
