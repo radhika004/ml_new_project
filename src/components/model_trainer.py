d32578e897794319df919abe00d66fa1718617d6
radhika004 <radhikachare2004@gmail.com>
2026-07-17 23:56:19 +0530

    data transformation completed:

---
+++ src/components/model_trainer.py
@@ -0,0 +1,58 @@
+import os 
+import sys 
+from  dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+  AdaBoostRegressor,
+  GradientBoostingRegressor,
+  RandomForestRegressor,
+)
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+
+from src.utils import save_object
+
+@dataclass
+class ModelTrainerConfig:
+  trained_model_file_path = os.path.join("artifact","model.pkl")
+  class ModelTrainer:
+    def __init__(self):
+      self.model_trainer_config = ModelTrainerConfig()
+      
+    def initiate_model_trainer(self,train_array,test_array,preprocesor_path):
+      try:
+        logging.info("Split training and testing data")
+        X_train,y_train,X_test,y_test=(
+          train_array[::-1],
+          train_array[:-1],
+          test_array[:,:-1],
+          test_array[:-1]
+          
+          
+        )
+        models = {
+          "Random Forest": RandomForestRegressor(),
+          "Decision Tree": DecisionTreeRegressor(),
+          "Gradient Boosting": GradientBoostingRegressor(),
+          "Linear Regression": LinearRegression(),
+          "K-Neighbors Regressor": KNeighborsRegressor(),
+          "XGBoost Regressor": XGBRegressor(),
+          "CatBoost Regressor": CatBoostRegressor(verbose=False),
+          "AdaBoost Regressor": AdaBoostRegressor(),
+        }
+        
+        
+        model_report : dict=evaluate_model(X=X_train)
+        
+        
+      except:
+        pass
+      
