92bb43c5a716e6e52b4ab22b2e753e7e87aa3332
radhika004 <radhikachare2004@gmail.com>
2026-07-19 20:42:00 +0530

    predict pipeline initiated

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,14 @@
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
+
+class PredictPipeline:
+  def __init__(self):
+    pass
+  
+class CustomData:
+  def __init__(self,
+               
+               gender: str,
+               race_ethini)
