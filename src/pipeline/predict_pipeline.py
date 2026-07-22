fbfc13dff6e77f7ffecb0142172c1c33d1dd1010
radhika004 <radhikachare2004@gmail.com>
2026-07-22 21:30:30 +0530

    prdiction pipeline

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -7,6 +7,27 @@ class PredictPipeline:
   def __init__(self):
     pass
   
+  def predict(self,features):
+    try:
+        model_path = 'artifact\\model.pkl'
+        preprocessor_path = 'artifact\\preprocessor.pkl'
+        model = load_object(file_path = model_path)
+        preprocessor = load_object(file_path = preprocessor)
+        data_scaled = preprocessor.transform(features)
+        preds = model.predict(data_scaled)
+        return preds
+    except Exception as e :
+        raise CustomException(e,sys)
+    
+    
+      
+      
+      
+
+  
+
+
+  
 class CustomData:
     def __init__(
         self,
