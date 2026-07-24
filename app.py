dca8cdec6100c883737c5f5d4a9236ac2125bcbc
radhika004 <radhikachare2004@gmail.com>
2026-07-24 22:33:51 +0530

    changes done

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application = Flask(__name__)
 
@@ -20,5 +21,13 @@ def predict_datapoint():
   if request.method=='GET':
     return render_template('home.html')
   else:
-    data = CustomData
-    10:35
+    data = data = CustomData(
+    gender=request.form.get('gender'),
+    race_ethnicity=request.form.get('ethnicity'),
+    parental_level_of_education=request.form.get('parental_level_of_education'),
+    lunch=request.form.get('lunch'),
+    test_preparation_course=request.form.get('test_preparation_course'),
+    reading_score=float(request.form.get('writing_score')),
+    writing_score=float(request.form.get('reading_score'))
+)
+    
