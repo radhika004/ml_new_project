4e945710b5c15afa14aa1ab0ecd7caa76726bfcd
radhika004 <radhikachare2004@gmail.com>
2026-08-01 21:31:25 +0530

    errors solved

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -12,7 +12,7 @@ class PredictPipeline:
         model_path = 'artifact\\model.pkl'
         preprocessor_path = 'artifact\\preprocessor.pkl'
         model = load_objects(file_path = model_path)
-        preprocessor = load_objects(file_path = preprocessor)
+        preprocessor = load_objects(file_path = preprocessor_path)
         data_scaled = preprocessor.transform(features)
         preds = model.predict(data_scaled)
         return preds
